8e1f67bc5fd3a01b40b2808c6c5bec34b70e7ce2
Clinton Danso <clint.danso@gmail.com>
2026-02-28 20:45:52 +0000

    removing email host settings

--- api/settings.py
+++ api/settings.py
@@ -136,12 +136,6 @@ EMAIL_BACKEND = (
     else "django.core.mail.backends.smtp.EmailBackend"
 )
 
-EMAIL_HOST = config("EMAIL_HOST", cast=str)
-EMAIL_PORT = config("EMAIL_PORT", cast=str)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str)
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
 
 ADMIN_USER_NAME = config("ADMIN_USER_NAME", cast=str, default="Admin User")
 ADMIN_USER_EMAIL = config("ADMIN_USER_EMAIL", cast=str, default=None)
