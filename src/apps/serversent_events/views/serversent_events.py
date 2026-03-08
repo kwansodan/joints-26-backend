7130ab18d3a21c1ed9141cb69dad58d410bd919d
Muftawu <mohammedyiwere@gmail.com>
2026-03-08 22:41:56 +0000

    refactored notify frontend view

--- src/apps/serversent_events/views/serversent_events.py
+++ src/apps/serversent_events/views/serversent_events.py
@@ -1,3 +1,5 @@
+import time
+
 from django.http import JsonResponse, StreamingHttpResponse
 from django.views import View
 from rest_framework_simplejwt.exceptions import TokenError
@@ -13,17 +15,27 @@ class NotifyFrontendView(View):
         pubsub = get_redis().pubsub()
         pubsub.subscribe(orderLocationUpdateChannel)
 
-        for message in pubsub.listen():
-            if message["type"] == "message":
-                print("received message", message)
-                yield f"data: {message['data'].decode()}\n\n"
+        while True:
+            message = pubsub.get_message(timeout=1)
+
+            if message and message["type"] == "message":
+                data = message["data"]
+                if isinstance(data, bytes):
+                    data = data.decode()
+
+                yield f"data: {data}\n\n"
+
             else:
                 yield ": keepalive\n\n"
 
+            time.sleep(1)
+
     def get(self, request):
         token = request.GET.get("token")
+
         if not token:
             return JsonResponse({"message": "invalid token"}, status=401)
+
         try:
             AccessToken(token=token, verify=True)
         except TokenError:
@@ -32,6 +44,12 @@ class NotifyFrontendView(View):
         response = StreamingHttpResponse(
             self._event_stream(), content_type="text/event-stream"
         )
+
         response["Cache-Control"] = "no-cache"
+        response["Connection"] = "keep-alive"
         response["X-Accel-Buffering"] = "no"
+        response["Content-Type"] = "text/event-stream"
+        response["Cache-Control"] = "no-cache"
+        response["Connection"] = "keep-alive"
+
         return response
