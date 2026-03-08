eb045071d546fda15d1cb8ee869f790740bed1a1
Muftawu <mohammedyiwere@gmail.com>
2026-03-08 19:08:28 +0000

    removed redis env db

--- src/services/server_sent_events.py
+++ src/services/server_sent_events.py
@@ -3,10 +3,12 @@ import json
 import redis
 from django.conf import settings
 
-REDIS_PUBSUB_HOST = settings.REDIS_PUBSUB_HOST
-REDIS_PUBSUB_DB_INDEX = settings.REDIS_PUBSUB_DB_INDEX
+DJANGO_ENV = settings.DJANGO_ENV
 
-rds = redis.Redis(host=REDIS_PUBSUB_HOST, port=6379, db=REDIS_PUBSUB_DB_INDEX)
+R_HOST = "redis" if DJANGO_ENV == "prod" else "127.0.0.1"
+R_DB_INDEX = 2
+
+rds = redis.Redis(host=R_HOST, port=6379, db=R_DB_INDEX)
 
 orderLocationUpdateChannel = "order_location_updates"
 
